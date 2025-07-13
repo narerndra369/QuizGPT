b6929e1ae5f3b194b4e3c2cd2c8a98d4badc7465
Malapula Narendra <141564331+narerndra369@users.noreply.github.com>
2025-07-13 23:43:32 +0530

    Delete QuizUI directory

--- QuizUI/Quiz/src/main/java/com/narendra/models/repository/ModelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.narendra.models.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.narendra.models.Quiz;
-
-public interface ModelRepository extends MongoRepository<Quiz, String> {
-    // You can add custom methods if needed
-}
